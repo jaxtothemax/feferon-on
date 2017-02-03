9ebb2c11bb3597909a0e0f68fe0832134bf7cf8a
jaxtothemax <jax.atlet@hotmail.com>
2017-02-03 12:08:26 +0100

    added css, images and js folders

---
+++ js/main.js
@@ -0,0 +1,4 @@
+TweenMax.from(".heading", 2, {opacity:0, scale:0, ease:Bounce.easeOut});
+TweenMax.staggerFrom(".Buttons", 1, {opacity:0, scale:0,y:400,x:0, ease:SlowMo.easeOut});
+TweenMax.staggerFrom(".iconSize", 1, {opacity:0, y:100, delay:1, rotation:360}, 0.5);
+TweenMax.staggerFrom(".iStockPhoto", 1, {opacity:0, y:100, delay:3.1}, 0.6);
